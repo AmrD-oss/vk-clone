e4a7d4c83244133c28569c8cd74d9ac4c2f78315
AmirDigiev <amir.digiev@gmail.com>
2020-03-25 23:48:23 +0300

    add deploy on Heroku

---
+++ src/main/java/com/example/socialnetwork/config/websocket/WebSocketAuthenticationConfig.java
@@ -0,0 +1,20 @@
+package com.example.socialnetwork.config.websocket;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+public class WebSocketAuthenticationConfig implements WebSocketMessageBrokerConfigurer {
+    
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+
+    }
+}
